ffccc3e3d49132e84d81cabecf3c0877b71c9e8b
Victor Daniel <99292901+vdanviel@users.noreply.github.com>
2025-11-12 23:38:19 +0000

    new version

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,7 +1,19 @@
-var flkty = new Flickity( '.projects', {
+new Flickity( '.works', {   
+    autoPlay: 1800,
     wrapAround: true,
-    autoPlay: true,
+    pauseAutoPlayOnHover: true,
+    freeScroll: true,
+    prevNextButtons: false,
+    pageDots: false,
+    cellAlign:'center',
+    contain: true
+});
+
+new Flickity( '.projects', {
+    wrapAround: true,
     freeScroll: true,
     prevNextButtons: true,
     cellAlign:'center'
 });
+
+
